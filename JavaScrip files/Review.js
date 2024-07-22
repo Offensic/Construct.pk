2673f70a05f94a6d124256cc742e9fc433d9e13c
Offensic <ziakhan0537@gmail.com>
2024-07-21 21:12:30 -0700

    Change in spelling

--- JavaScrip files/Review.js
+++ JavaScrip files/Review.js
@@ -4,12 +4,12 @@
 
 const Reviewcontainer = document.querySelector('#Review-container');
 const Reviews = [
-    { image: './assest/15.jpg', name: 'Hary',         para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
-    { image: './assest/11.jpg', name: 'Jack Sparrow', para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
-    { image: './assest/1.jpg', name: 'Mork',    para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
-    { image: './assest/12.jpg', name: 'Waleed', para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
-    { image: './assest/16.jpg', name: 'Jakson', para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
-    { image: './assest/17.jpg', name: 'Neymar', para: '❝ Very happy with the team from Pakistan. Aestetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/15.jpg', name: 'Hary',         para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/11.jpg', name: 'Jack Sparrow', para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/1.jpg', name: 'Mork',    para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/12.jpg', name: 'Waleed', para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/16.jpg', name: 'Jakson', para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
+    { image: './assest/17.jpg', name: 'Neymar', para: '❝ Very happy with the team from Pakistan. Aesthetic Homes for Providing us with a smooth and trouble-free planning stage for oour dream home. Our house was fully custom-built and homes were able to satisfy our needs. ❞' },
 
 
 ]
